8e722b244b5815ee18f5aa4511a37176fe2dbf95
Kiryushkin <Kiryushkin73@gmail.com>
2021-07-21 13:31:18 +0300

    complete services and services_page

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ const browserTask = () =>
 const watcher = () => {
   watch('./src/scss/**/*.scss', sassTask);
   watch('./src/about/scss/**/*.scss', sassTaskAbout);
-  watch('./src/about/scss/**/*.scss', sassTaskServices);
+  watch('./src/services/scss/**/*.scss', sassTaskServices);
+  watch('./src/services/services_page/scss/**/*.scss', sassTaskServices_page);
   watch('./src/*.html').on('change', browserSync.reload);
 };
 
@@ -32,6 +33,12 @@ const sassTaskServices = () =>
   src('./src/services/scss/style.scss')
     .pipe(sass())
     .pipe(dest('./src/services/css'))
+    .pipe(browserSync.stream());
+
+const sassTaskServices_page = () =>
+  src('./src/services/services_page/scss/style.scss')
+    .pipe(sass())
+    .pipe(dest('./src/services/services_page/css'))
     .pipe(browserSync.stream());
 
 exports.default = series(sassTask, parallel(browserTask, watcher));
